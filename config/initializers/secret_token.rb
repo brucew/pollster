835e2d9b2a375ce5485ed41c5ed0ae68fd8dc1ff
Bruce White <bruce@homecca.com>
2013-02-15 15:23:48 -0700

    Initial setup Install RSpec Install Cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollster::Application.config.secret_token = '51f410e863732703a4d11f25c7e6414229eeefb0546cb986ef388ca38f588fe86f59c6fa45bb9da8c8ef367e992042f0546859d24b1681735cae21cf848a2627'
