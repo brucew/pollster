e30dff9e0eeca60884a4bfae41b0f50df1f02748
Bruce White <bruce@homecca.com>
2013-02-17 17:33:36 -0700

    Prettied UI using Bootstrap and SimpleForm Add unique vote IP address per poll validation

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,17 +1,24 @@
 require 'spec_helper'
 
 describe Vote do
-  it "should create a new instance given valid attributes" do
+  it 'should create a new instance given valid attributes' do
     FactoryGirl.create(:vote)
   end
 
-  it "should require an ip_address" do
+  it 'should require an IP address' do
     vote = FactoryGirl.build(:vote, ip_address: nil)
     vote.should_not be_valid
   end
 
-  it "should require an item" do
+  it 'should require an item' do
     vote = FactoryGirl.build(:vote, item: nil)
+    vote.should_not be_valid
+  end
+
+  it 'should be unique for the IP address per poll' do
+    poll = FactoryGirl.create(:poll)
+    FactoryGirl.create(:vote, item: poll.items.first, ip_address: '0.0.0.0')
+    vote = FactoryGirl.build(:vote, item: poll.items.first, ip_address: '0.0.0.0')
     vote.should_not be_valid
   end
 
