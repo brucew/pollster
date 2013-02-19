2b4ae886c0580dabc26f09ed0ec3dd42c7f746f4
Bruce White <bruce@homecca.com>
2013-02-18 21:38:47 -0700

    Fix vote race condition bug

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,7 @@ class Vote < ActiveRecord::Base
   belongs_to :item, inverse_of: :votes
   belongs_to :poll, inverse_of: :votes
 
+  validates :poll, presence: true
   validates :item, presence: true
   validates :ip_address, presence: true, uniqueness: { scope: :poll_id }
 
@@ -9,4 +10,10 @@ class Vote < ActiveRecord::Base
     self.poll = item.poll unless item.nil?
     super item
   end
+
+  def item_id=(item_id)
+    self.poll = Item.find(item_id).poll unless item_id.nil?
+    super item_id
+  end
+
 end
